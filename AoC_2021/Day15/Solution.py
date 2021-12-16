e459803d2912eca5ec76e30d9e051ea1a8994e46
Jessica Moore <jmoore523@gmail.com>
2021-12-15 22:30:22 -0500

    Clean up

--- AoC_2021/Day15/Solution.py
+++ AoC_2021/Day15/Solution.py
@@ -19,23 +19,22 @@ def solve(risk_levels: np.ndarray, part2=False) -> int:
             tiles.append(np.hstack(tiles_i))
         risk_levels = np.vstack(tiles)
 
-
     lowest_costs = np.ones_like(risk_levels) * sys.maxsize
     lowest_costs[0][0] = 0
     queue = []
     heapq.heappush(queue, (0, 0, 0))
     while True:
-        _, x0, y0 = heapq.heappop(queue)
-        c = lowest_costs[y0][x0]
-        if y0 == len(risk_levels) - 1 and x0 == len(risk_levels[y0]) - 1:
+        _, x, y = heapq.heappop(queue)
+        c = lowest_costs[y][x]
+        if y == len(risk_levels) - 1 and x == len(risk_levels[y]) - 1:
             return c
-        for x1, y0 in ((x0 - 1, y0), (x0, y0 - 1), (x0, y0 + 1), (x0 + 1, y0)):
-            if y0 not in range(len(risk_levels)) or x1 not in range(len(risk_levels[y0])):
+        for x1, y1 in ((x - 1, y), (x, y - 1), (x, y + 1), (x + 1, y)):
+            if y1 not in range(risk_levels.shape[1]) or x1 not in range(risk_levels.shape[0]):
                 continue
-            d = c + risk_levels[y0][x1]
-            if d < lowest_costs[y0][x1]:
-                lowest_costs[y0][x1] = d
-                heapq.heappush(queue, (d, x1, y0))
+            d = c + risk_levels[y1][x1]
+            if d < lowest_costs[y1][x1]:
+                lowest_costs[y1][x1] = d
+                heapq.heappush(queue, (d, x1, y1))
 
 
 with open("./Day15Input.txt") as f:
