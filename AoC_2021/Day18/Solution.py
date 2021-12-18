61731e96a1c3023befc34bad32e4ed22a23a1586
Jessica Moore <jmoore523@gmail.com>
2021-12-18 07:30:50 -0500

    Implement explode

--- AoC_2021/Day18/Solution.py
+++ AoC_2021/Day18/Solution.py
@@ -1,5 +1,65 @@
 import json
-from typing import List
+from typing import List, Tuple
+
+
+def find_next_num_loc(snailfish_number: str, backward=False):
+    start, end, step = 0, len(snailfish_number), 1
+    if backward:
+        start, end, step = len(snailfish_number) - 1, 0, -1
+    for i in range(start, end, step):
+        el = snailfish_number[i]
+        if el.isnumeric():
+            return i
+
+
+def explode(snailfish_number: str) -> Tuple[int, int]:
+    open_brackets_count = 0
+    for i, el in enumerate(snailfish_number):
+        if el == '[':
+            open_brackets_count += 1
+            if open_brackets_count == 5:
+                if snailfish_number[i + 1].isnumeric():
+                    idx_s, idx_e = i, i + 5
+                    pre, post = snailfish_number[:idx_s], snailfish_number[idx_e:]
+                    x, y = json.loads(snailfish_number[idx_s: idx_e])
+
+                    left_num_loc = find_next_num_loc(pre, backward=True)
+                    if left_num_loc is not None:
+                        new_num = x + int(pre[left_num_loc])
+                        pre = pre[0:left_num_loc] + str(new_num) + pre[left_num_loc+1:]
+                    
+                    right_num_loc = find_next_num_loc(post)
+                    if right_num_loc is not None:
+                        new_num = y + int(post[right_num_loc])
+                        post = post[0:right_num_loc] + str(new_num) + post[right_num_loc+1:]
+
+                    snailfish_number = pre + '0' + post
+                    return snailfish_number
+        
+        elif el == ']':
+            open_brackets_count -= 1
+
+
+def snailfish_reduce(snailfish_number: str) -> str:
+    while True:
+        # If any pair is nested inside four pairs
+        snailfish_number = explode(snailfish_number)
+        return snailfish_number
+
+
+def snailfish_addition(to_add: List[str]) -> str:
+    current_number = ''
+    for i, el in enumerate(to_add):
+        current_number += ','
+        current_number += el
+        if i == 0:
+            current_number = current_number[1:]
+            continue
+
+        current_number = snailfish_reduce(current_number)
+
+    import pdb
+    pdb.set_trace()
 
 
 def find_magnitude(snailfish_number: List) -> int:
@@ -20,6 +80,6 @@ def find_magnitude(snailfish_number: List) -> int:
 
 
 with open("./Day18Input.txt") as f:
-    puzzle_input = f.read().strip()
+    puzzle_input = [x.strip() for x in f.readlines()]
 
-print(find_magnitude(json.loads(puzzle_input)))
+print(snailfish_reduce(puzzle_input[0]))
